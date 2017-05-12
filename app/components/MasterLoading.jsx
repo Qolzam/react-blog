0474a6c41693efb48969a7d58b4c2823f21ffbab
amir qolzam <Qolzam@amirs-MacBook-Pro.local>
2017-05-12 21:38:33 +0430

    change master loading color

--- app/components/MasterLoading.jsx
+++ app/components/MasterLoading.jsx
@@ -26,7 +26,7 @@ export default class MasterLoading extends Component {
     return (
 
       <Dimmer page active={this.props.activeLoading} style={{
-        background: "black"
+        background: "white"
       }} inverted onClickOutside={this.handleShow}>
 
         <i className="huge icons">
@@ -34,18 +34,7 @@ export default class MasterLoading extends Component {
           <i className="suitcase teal icon"></i>
         </i>
         <Header as='h2'>
-<<<<<<< HEAD
             Green's Diary
-=======
-          I
-
-<svg class="heart" viewBox="0 0 32 29.6">
-  <path d="M23.6,0c-3.4,0-6.3,2.7-7.6,5.6C14.7,2.7,11.8,0,8.4,0C3.8,0,0,3.8,0,8.4c0,9.4,9.5,11.9,16,21.2
-	c6.1-9.3,16-12.1,16-21.2C32,3.8,28.2,0,23.6,0z"/>
-</svg>
-
-You
->>>>>>> changed package.json
           <Header.Subheader>
             Loading...
           </Header.Subheader>
